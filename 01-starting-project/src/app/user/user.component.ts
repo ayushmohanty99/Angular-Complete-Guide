c61633ddc68fe491e6a9115abdf680c53c1de0e2
Ayush Mohanty <ayushm.dev@protonmail.com>
2024-10-05 01:27:34 -0400

    40. TS: Type Aliases & Interfaces

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,12 @@
 import { Component, computed, EventEmitter, input, Input, output, Output} from '@angular/core';
 
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,16 +15,14 @@ import { Component, computed, EventEmitter, input, Input, output, Output} from '
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string; 
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return '../assets/users/' + this.avatar;
+    return '../assets/users/' + this.user.avatar;
   }
   
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
